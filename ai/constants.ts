6207ce246837ac9e35c9c5e282279d68ce491b60
qwertyczee <kumzak.maty@gmail.com>
2025-10-07 18:16:34 +0200

    Chnaged model

--- ai/constants.ts
+++ ai/constants.ts
@@ -1,7 +1,7 @@
 export enum Models {
     OpenAIGPT5 = 'openai/gpt-5',
     OpenAIGPT5Mini = 'openai/gpt-5-mini',
-    XaiGrok4Fast = 'x-ai/grok-4-fast',
+    XaiGrokCodeFast = 'x-ai/grok-code-fast-1',
     ZaiGLM46 = 'z-ai/glm-4.6',
     AnthropicClaude45Sonnet = 'anthropic/claude-sonnet-4.5',
     GoogleGeminiFlash = 'google/gemini-2.5-flash',
@@ -12,7 +12,7 @@ export const DEFAULT_MODEL = Models.OpenAIGPT5;
 export const SUPPORTED_MODELS: string[] = [
     Models.OpenAIGPT5,
     Models.OpenAIGPT5Mini,
-    Models.XaiGrok4Fast,
+    Models.XaiGrokCodeFast,
     Models.ZaiGLM46,
     Models.AnthropicClaude45Sonnet,
     Models.GoogleGeminiFlash,
